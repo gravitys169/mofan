d88ee215c96846eb41ba13e2cfc986a9e3e342ef
gravitys169 <314379376@qq.com>
2017-10-26 11:55:11 +0800

    all test delete

--- lesson5.py
+++ lesson5.py
@@ -9,9 +9,6 @@ import numpy as np
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
 
-#git succeed
-this should be delete
-third time 
 ### create tensorflow structure start ###
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases = tf.Variable(tf.zeros([1]))
